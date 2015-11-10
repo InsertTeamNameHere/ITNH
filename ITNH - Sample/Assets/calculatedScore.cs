1e0d3521197feed14d9a0daba750805efdd8a98f
Andy de Caussin <decau101@mail.chapman.edu>
2015-11-10 15:05:58 -0800

    Calculation of score implemented
    Need to work on making it appear and disappear properly

---
+++ ITNH - Sample/Assets/calculatedScore.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class calculatedScore : MonoBehaviour {
+
+	private Score score;
+
+	private int left;
+	private int total;
+
+	public int finalScore;
+
+	private TextMesh scoreText;
+
+	// Use this for initialization
+	void Start () {
+		score = GameObject.FindGameObjectWithTag ("world").GetComponent<Score> ();
+		scoreText = GameObject.FindGameObjectWithTag ("score box").GetComponent<TextMesh> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		left = (int)score.Left;
+		total = (int)score.Total;
+
+		if (left == 0) {
+			finalScore = total * 100;
+			finalScore = finalScore + (total * 10);
+		}
+		else{
+			finalScore = (total * 100) - (left * 100);
+		}
+
+		scoreText.text = "Score: " + (int)finalScore;
+	}
+}
