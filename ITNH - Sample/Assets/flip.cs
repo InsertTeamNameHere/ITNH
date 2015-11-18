c599a5fa35e2196317ca941fff3409f81f73473e
afi7259 <lohr.jeff@gmail.com>
2015-11-17 20:27:22 -0800

    fix start menu
    fix start menu

---
+++ ITNH - Sample/Assets/flip.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (180 * Time.deltaTime, 0, 0);
+	}
+}
